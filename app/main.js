8a317e971c1c6b0f4724cfb74c696b07cc614733
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-27 10:03:09 -0300

    metodo map()

--- app/main.js
+++ app/main.js
@@ -7,7 +7,8 @@ async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
     console.table(livros)
-    exibirOsLivrosNaTela(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
 
 
